3fd0f7ae75cd6130d7940016c7cd46e349531acf
Preethi Prerana <preethi_prerana@thbs.india.com>
2024-02-01 12:07:23 +0530

    login and registration using springboot

---
+++ src/main/java/com/example/demo/service/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.example.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.example.demo.CustomUserDetails;
+import com.example.demo.User;
+import com.example.demo.UserRepository;
+ 
+public class CustomUserDetailsService implements UserDetailsService {
+ 
+    @Autowired
+    private UserRepository userRepo;
+     
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new CustomUserDetails(user);
+    }
+ 
+}
